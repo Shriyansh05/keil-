b21f146485f9ebe3f540738889be4bdd54ff3e10
Shriyansh05 <70624481+Shriyansh05@users.noreply.github.com>
2020-09-09 21:46:29 +0530

    lab 1

---
+++ lab1/Objects/addition.d
@@ -0,0 +1,3 @@
+.\objects\addition.o: addition.s
+.\objects\addition.o: MSP432xx_constants.s
+.\objects\addition.o: MSP432xx_tim_constants.s
---
+++ lab1/Objects/simple_addition.d
@@ -0,0 +1,3 @@
+.\objects\simple_addition.o: SImple_Addition.s
+.\objects\simple_addition.o: MSP432xx_constants.s
+.\objects\simple_addition.o: MSP432xx_tim_constants.s
